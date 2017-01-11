bd4be62b8d417e1dde9bc13dcc6bee05451aa0f9
Eli Baum <elimbaum@gmail.com>
2017-01-11 01:23:12 -0500

    very simple; just plants trees! and chases bad guys.

--- src/helloworldeli/RobotPlayer.java
+++ src/helloworldeli/RobotPlayer.java
@@ -15,9 +15,10 @@ public strictfp class RobotPlayer {
 				runArchon();
 				break;
 			case GARDENER:
-				//runGardener();
+				runGardener();
 				break;
 			case SOLDIER:
+				runSoldier();
 				break;
 			case LUMBERJACK:
 				break;
@@ -41,10 +42,10 @@ public strictfp class RobotPlayer {
 					rc.move(dir);
 				}
 				
-//				if(rc.canHireGardener(dir.opposite()))
-//				{
-//					rc.hireGardener(dir.opposite());
-//				}
+				if(rc.canHireGardener(dir.opposite()))
+				{
+					rc.hireGardener(dir.opposite());
+				}
 				
 				Clock.yield();
 			}
@@ -56,33 +57,96 @@ public strictfp class RobotPlayer {
 		}
 	}
 	
-//	static void runGardener() throws GameActionException
-//	{
-//		while (true)
-//		{
-//			try
-//			{
-//				Direction dir = randomDirection();
-//				
-//				if(rc.canMove(dir))
-//				{
-//					rc.move(dir);
-//				}
-//				
-//				if(rc.canPlantTree(dir.opposite()))
-//				{
-//					rc.plantTree(dir.opposite());
-//				}
-//				
-//				Clock.yield();
-//			}
-//			catch (Exception e)
-//			{
-//				System.out.println("Gardener died");
-//				e.printStackTrace();
-//			}
-//		}
-//	}
+	static void runGardener() throws GameActionException
+	{
+		while (true)
+		{
+			try
+			{
+				Direction dir = randomDirection();
+				
+				if(rc.canPlantTree(dir.opposite()) && Math.random() < 0.1)
+				{
+					rc.plantTree(dir.opposite());
+				}
+				else if(rc.canBuildRobot(RobotType.SOLDIER, dir.opposite()))
+				{
+					rc.buildRobot(RobotType.SOLDIER, dir.opposite());
+				}
+				
+				// tree maintenance
+				TreeInfo[] trees = rc.senseNearbyTrees(-1, rc.getTeam());
+				
+				for(TreeInfo t : trees)
+				{	
+					if (rc.canWater(t.ID))
+					{
+						rc.water(t.ID);
+						//System.out.println("Watered " + t.ID);
+					}
+					if (rc.canShake(t.ID))
+					{
+						rc.shake(t.ID);
+						//System.out.println("Shook " + t.ID);
+					}
+				}
+				
+				if(rc.canMove(dir))
+				{
+					rc.move(dir);
+				}
+				
+				Clock.yield();
+			}
+			catch (Exception e)
+			{
+				System.out.println("Gardener died");
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	static void runSoldier() throws GameActionException
+	{
+		Team enemy = rc.getTeam().opponent();
+		
+		while(true)
+		{
+			try
+			{
+				MapLocation myLoc = rc.getLocation();
+				
+				RobotInfo[] robots = rc.senseNearbyRobots(-1, enemy);
+				
+				Direction dir;
+				if (robots.length > 0)
+				{
+					dir = myLoc.directionTo(robots[0].location);
+					if (rc.canFireTriadShot())
+					{
+						rc.fireTriadShot(dir);
+						dir.rotateLeftDegrees(15);
+					}
+					
+				}
+				else
+				{
+					dir = randomDirection();
+				}
+				
+				if(rc.canMove(dir))
+				{
+					rc.move(dir);
+				}
+				Clock.yield();
+			}
+			catch (Exception e)
+			{
+				System.out.println("Solider died.");
+				e.printStackTrace();
+			}
+		}
+	}
 	
     static Direction randomDirection() {
         return new Direction((float)Math.random() * 2 * (float)Math.PI);
