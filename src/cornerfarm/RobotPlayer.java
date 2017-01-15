c3787266d94e6c6bf308fe4f0ce87a56744f0670
Eli Baum <elimbaum@gmail.com>
2017-01-15 01:33:40 -0500

    YEAHHH the farming works! not super great on real maps with neutral trees, but works on empty map.

--- src/cornerfarm/RobotPlayer.java
+++ src/cornerfarm/RobotPlayer.java
@@ -148,9 +148,10 @@ public class RobotPlayer {
             				(rc.canSenseAllOfCircle(gridLoc,1) && rc.isCircleOccupiedExceptByThisRobot(gridLoc, 1)) ||
             				(rc.canSenseLocation(treeSpot) && rc.isLocationOccupiedByTree(treeSpot))) {
             			// need new location!
+            			
             			System.out.println(gridLoc);
             			oldLoc = gridLoc;
-            			gridLoc = gridLoc.translate(5 * (1 - (int) (Math.random() * 3)), 5 * (1 - (int) (Math.random() * 3)));
+            			gridLoc = nextGridloc(gridLoc, archonLoc);
             			System.out.println(gridLoc);
             			newLocationRequest = false;
             		}
@@ -197,8 +198,8 @@ public class RobotPlayer {
                 		{
                 			rc.move(dir);
                 		}
-            			gridLoc = oldLoc;
-            			newLocationRequest = true;
+            			//gridLoc = oldLoc;
+            			//newLocationRequest = true;
             		}
             		
             	}
@@ -291,5 +292,64 @@ public class RobotPlayer {
 
     static Direction randomDirection() {
         return new Direction((float)Math.random() * 2 * (float)Math.PI);
+    }
+    
+    static MapLocation nextGridloc(MapLocation current, MapLocation target) throws Exception
+    {
+    	// Compute weights
+    	
+    	// Stores CDF (loop across row first, then down column)
+    	double[][] weights = new double[3][3];
+    	float sumWeights = 0;
+		for(int i = -1; i <= 1; i++)
+		{
+			for(int j = -1; j <= 1; j++)
+			{
+				double dx = constrain(target.x, current.x - 5, current.x + 5) - (current.x + 5 * i);
+				double dy = constrain(target.y, current.y - 5, current.y + 5) - (current.y + 5 * j);
+				
+				// remaining on current tile is not an option
+				if(i == 0 && j == 0)
+				{
+					System.out.format("Reference dx=%2.0f dy=%2.0f%n", dx, dy);
+					continue;
+				}
+			
+				
+				// this weight is the PDF for each of the eight directions
+				double weight = 0.5 + Math.atan((dx * (current.x - target.x) + dy * (current.y - target.y)) / 100) / Math.PI;
+				
+				System.out.format("(%2d,%2d) dx=%2.0f dy=%2.0f -> %f%n", i,j,dx,dy,weight);
+				sumWeights += weight;
+				weights[i + 1][j + 1] = sumWeights;
+			}
+		}
+		
+    	// pick a direction!
+		double r = Math.random() * sumWeights;
+		
+		for(int i = -1; i <= 1; i++)
+		{
+			for(int j = -1; j <= 1; j++)
+			{
+				if (i == 0 && j == 0) continue;
+				
+				if (weights[i + 1][j + 1] >= r)
+				{
+					// found it!
+					return current.translate(5 * i, 5 * j);
+				}
+			}
+		}
+		
+		throw new Exception("CDF Failed");
+		
+    }
+    
+    static double constrain(double x, double min, double max)
+    {
+    	if (x < min) return min;
+    	else if(x > max) return max;
+    	return x;
     }
 }
